f9304461072df75f47ce6657e946591b83b32aff
kauan araujo <kauanraujo340@gmail.com>
2025-04-25 11:09:46 -0300

    erro data-testid

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,5 +1,4 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
 import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
@@ -7,4 +6,27 @@ describe('Teste para o componente PostComment', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
+
+    it('deve adicionar dois comentarios', () => {
+        render(<PostComment/>);
+
+        fireEvent.change(screen.getByTestId('comment-textarea'), {
+            target:{
+                value: 'comentario adicionado teste',
+            }
+        });
+
+        fireEvent.click(screen.getByTestId('comment-button'));
+
+        fireEvent.change(screen.getByTestId('comment-textarea'),{
+            target:{
+                value:'segundo comentario adicionado teste',
+            }
+        });
+
+        fireEvent.click(screen.getByTestId('comment-button'));
+
+        expect(screen.getByTestId('comment-element')).toHaveLength(2);
+    });
 });
+
